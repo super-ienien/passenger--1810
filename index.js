380a22d2c51974b7c19e47063b3f0cfc0c274f64
vivien anglesio <vivien.anglesio@gmail.com>
2016-05-22 16:03:26 +0200

    fix server creation and add some log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Setup basic express server
-var server = require('http').createServer(app);
+var server = require('http').createServer(function (){});
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
@@ -11,13 +11,15 @@ var numUsers = 0;
 
 io.on('connection', function (socket) {
     numUsers++;
-
+    var connectionNum = numUsers;
+    console.log ("Socket "+connectionNum+" - connected");
     socket.on('ping', function (data) {
-
+        console.log ("Socket "+connectionNum+" - sent ping");
         socket.emit('pong', data);
     });
 
     socket.on('disconnect', function () {
+        console.log ("Socket "+connectionNum+" - disconnected");
         numUsers--;
     });
 });
